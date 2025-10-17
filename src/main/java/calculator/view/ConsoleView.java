fef416769bf82947409207d8b51f539f9af76089
Chaemin Yooun <cinnamein@gmail.com>
2025-10-17 16:52:37 +0900

    feat(ConsoleView): 결과 출력 기능 추가

--- src/main/java/calculator/view/ConsoleView.java
+++ src/main/java/calculator/view/ConsoleView.java
@@ -1,11 +1,17 @@
 package calculator.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.math.BigDecimal;
 
 public class ConsoleView {
 
     public String consoleInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine();
+    }
+
+    public void consoleOutput(BigDecimal result) {
+        System.out.println("결과 : " + result);
+        Console.close();
     }
 }
